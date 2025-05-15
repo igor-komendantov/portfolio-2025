5fefaf95662c0019f1b7deb990cb7f8803c4cda4
Wirbelwind <komendantov2580@gmail.com>
2025-05-15 23:07:02 +0300

    create component for rendering of book

---
+++ components/BookModel.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+const {
+  scene: model,
+  animations,
+} = await useGLTF("/models/book.glb");
+
+const { actions, mixer } = useAnimations(animations, model);
+</script>
+
+<template>
+  <primitive :object="model" />
+</template>
