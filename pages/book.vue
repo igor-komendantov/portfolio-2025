af893ce3b2991a6f338f472e5a818327309b9a33
Wirbelwind <komendantov2580@gmail.com>
2025-05-15 23:08:03 +0300

    render a scene

--- pages/book.vue
+++ pages/book.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { TresCanvas } from "@tresjs/core";
+import { OrbitControls } from "@tresjs/cientos";
+</script>
+
 <template>
-    <h1>book</h1>
+  <TresCanvas clear-color="#DD9D6F" window-size>
+    <TresPerspectiveCamera :position="[0, 1.2, 0]" :look-at="[0, 0, 0]" />
+    <OrbitControls />
+
+    <Suspense>
+      <BookModel />
+    </Suspense>
+  </TresCanvas>
 </template>
