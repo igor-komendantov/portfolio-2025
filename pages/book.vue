b90af3278338b1b9d3f967ada974843eb1950a0d
Wirbelwind <komendantov2580@gmail.com>
2025-05-15 23:08:16 +0300

    add a light to scene

--- pages/book.vue
+++ pages/book.vue
@@ -8,6 +8,14 @@ import { OrbitControls } from "@tresjs/cientos";
     <TresPerspectiveCamera :position="[0, 1.2, 0]" :look-at="[0, 0, 0]" />
     <OrbitControls />
 
+    <!-- Light -->
+    <TresAmbientLight :intensity="0.7" />
+
+    <TresDirectionalLight :position="[3, 5, 2]" :intensity="2" cast-shadow />
+    <TresDirectionalLight :position="[-3, 2, -2]" :intensity="0.8" />
+    <TresDirectionalLight :position="[0, 5, -5]" :intensity="0.5" />
+    <!-- Light -->
+
     <Suspense>
       <BookModel />
     </Suspense>
