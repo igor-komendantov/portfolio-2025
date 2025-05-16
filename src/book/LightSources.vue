95f34c39ac7f53f292322c2200c3c57624abf3d0
Wirbelwind <komendantov2580@gmail.com>
2025-05-16 17:36:52 +0300

    create component LightSources

---
+++ src/book/LightSources.vue
@@ -0,0 +1,7 @@
+<template>
+  <TresAmbientLight :intensity="0.7" />
+
+  <TresDirectionalLight :position="[3, 5, 2]" :intensity="2" cast-shadow />
+  <TresDirectionalLight :position="[-3, 2, -2]" :intensity="0.8" />
+  <TresDirectionalLight :position="[0, 5, -5]" :intensity="0.5" />
+</template>
