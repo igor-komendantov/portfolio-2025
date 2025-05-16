a030d711a4b77078e0c07f4975e72177a5356d9d
Wirbelwind <komendantov2580@gmail.com>
2025-05-16 17:36:43 +0300

    create component PageNavigation

---
+++ src/book/PageNavigation.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Html } from '@tresjs/cientos';
+
+</script>
+
+<template>
+    <Html :position="[-1, 0, 0]">
+      <img
+        src="https://placehold.co/600x400/EEE/31343C"
+        width="100"
+        height="100"
+      />
+    </Html>
+
+    <Html :position="[0.9, 0, 0]">
+      <img
+        src="https://placehold.co/600x400/EEE/31343C"
+        width="100"
+        height="100"
+      />
+    </Html>
+</template>
