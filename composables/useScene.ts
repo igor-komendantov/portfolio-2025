ff84ca1d46e00bb6e384703b93983ff7aa7c054e
Wirbelwind <komendantov2580@gmail.com>
2025-05-17 20:10:59 +0300

    create state pageStep

--- composables/useScene.ts
+++ composables/useScene.ts
@@ -1,3 +1,7 @@
-export const useScene = () => useState("scene", () => ({
-  showNavigation: false,
-}));
+export const useScene = () => {
+  return useState("scene", () => ({
+    showNavigation: false,
+    // this field indicates about 2 currently visible pages of book
+    pageStep: 0,
+  }));
+};
