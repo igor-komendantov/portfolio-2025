630a3c8637f19e9c870ade461aac537bee0bbeaa
Wirbelwind <komendantov2580@gmail.com>
2025-05-18 23:53:53 +0300

    decrease light sources

--- components/LightSources.vue
+++ components/LightSources.vue
@@ -2,6 +2,6 @@
   <TresAmbientLight :intensity="0.7" />
 
   <TresDirectionalLight :position="[3, 5, 2]" :intensity="2" cast-shadow />
-  <TresDirectionalLight :position="[-3, 2, -2]" :intensity="0.8" />
-  <TresDirectionalLight :position="[0, 5, -5]" :intensity="0.5" />
+  <!-- <TresDirectionalLight :position="[-3, 2, -2]" :intensity="0.8" /> -->
+  <!-- <TresDirectionalLight :position="[0, 5, -5]" :intensity="0.5" /> -->
 </template>
