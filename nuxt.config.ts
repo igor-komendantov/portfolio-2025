a89bee082a208aacea15c0bce573b45477fcf1e1
Wirbelwind <komendantov2580@gmail.com>
2025-05-30 13:32:54 +0300

    add links to .env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@tresjs/nuxt']
+  modules: ['@nuxt/eslint', '@tresjs/nuxt'],
+  runtimeConfig: {
+    public: {
+      email: 'placeholder',
+      linkedIn: 'placeholder',
+      github: 'placeholder'
+    }
+  }
 })
